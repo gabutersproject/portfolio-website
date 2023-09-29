726876a0573a2822c1eff4e3430ce429c429c508
MhmmdAmier <dewa27bkz@gmail.com>
2023-09-29 23:00:50 +0700

    again
    Signed-off-by: MhmmdAmier <dewa27bkz@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,18 +43,6 @@ export const metadata: Metadata = {
     type: 'website',
   },
   metadataBase: new URL('https://mhmmdamir.biz.id'),
-  icons: {
-    icon: [
-      '/favicon.ico?v=4',
-    ],
-    apple: [
-      '/apple-touch-icon.png?v=4',
-    ],
-    shortcut: [
-      '/apple-touch-icon.png',
-    ],
-  },
-  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
